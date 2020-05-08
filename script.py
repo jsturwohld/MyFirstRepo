0f4a0f906a77b35e69f081d37e789f6a554f1dda
Jason Sturwohld <jsturwohld@bigpond.com>
2020-05-08 13:46:45 +1000

    Testing Git functionality

--- script.py
+++ script.py
@@ -8,5 +8,5 @@ def greet(who_to_greet):
     greeting = 'Hello, {}'.format(who_to_greet)
     return greeting
 
-print(greet('World'))
-print(greet('Corey'))
+print(greet('Everybody!'))
+print(greet('Jason'))
